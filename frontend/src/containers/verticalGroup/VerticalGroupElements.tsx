280d69ef9ec2779f6ba0b11f473a450674182854
Nazar Derevetskyy <nazmouse22@hotmail.com>
2022-06-09 23:16:48 -0400

    Get rid of vertical group since MUI is used, it has it's own containers

--- frontend/src/containers/verticalGroup/VerticalGroupElements.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  margin: 1em auto;
-  row-gap: 0.5em;
-`
