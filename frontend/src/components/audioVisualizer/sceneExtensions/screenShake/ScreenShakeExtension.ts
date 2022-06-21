90486c1fff489cfd137cf421f4bd2914b89373fa
Nazar Derevetskyy <nazmouse22@hotmail.com>
2022-06-20 23:30:13 -0400

    Screenshake extension

--- frontend/src/components/audioVisualizer/sceneExtensions/screenShake/ScreenShakeExtension.ts
+++ frontend/src/components/audioVisualizer/sceneExtensions/screenShake/ScreenShakeExtension.ts
@@ -1,13 +1,83 @@
 import { SceneExtension } from "../SceneExtension";
-import { Scene } from "@babylonjs/core";
+import { Matrix, Mesh, Scene, Vector4 } from "@babylonjs/core";
 import { AudioInput } from "../../AudioInput";
+import { Modifiers } from "../../../../Constants";
+
+enum ShakeMode { Single, Group }
 
 export class ScreenShakeExtension extends SceneExtension {
-  initialize(): void {
+  private static _ProjectionMatrixCache: Matrix|undefined;
 
+  private _r: Vector4 = new Vector4(0,0,0,0);
+  private _t: number = 0;
+  private _projectionMatrix: Matrix = new Matrix();
+
+  constructor(inputData: any) {
+    super(inputData);
+    this._inputData = inputData[Modifiers.SCREENSHAKE_KEY];
   }
 
-  onBeforeSceneRender(scene: Scene, audioData: AudioInput): void {
+  initialize(scene: Scene): void {
+    if(!scene._activeCamera) return;
 
+    this._projectionMatrix = scene._activeCamera.getProjectionMatrix() || new Matrix();
+
+    // restore projection matrix on component modification to avoid camera translations
+    if(ScreenShakeExtension._ProjectionMatrixCache) {
+      const row: Vector4 = ScreenShakeExtension._ProjectionMatrixCache.getRow(3) || Vector4.Zero();
+      this._projectionMatrix.setRow(3, row);
+    }
+
+    // cache projection matrix if we have any available
+    if(!ScreenShakeExtension._ProjectionMatrixCache && this._projectionMatrix) {
+      ScreenShakeExtension._ProjectionMatrixCache = this._projectionMatrix.clone();
+    }
+
+    this._r = this._projectionMatrix.getRow(3) || Vector4.Zero();
+  }
+
+  onBeforeSceneRender(scene: Scene, visuals: Mesh[], audioData: AudioInput): void {
+    if (!this._inputData["active"]) return;
+
+    const mode = this.getModeFromString(this._inputData["mode"] || "single");
+    const modeMultiplier = this.calculateModeMultiplier(mode, visuals.length, audioData);
+
+    const x = (this._inputData["min"] || 0) * modeMultiplier;
+    const y = (this._inputData["max"] || 0) * modeMultiplier;
+
+    this._r.x += Math.cos(this._t) * x;
+    this._r.y += Math.sin(this._t) * y;
+    this._projectionMatrix.setRowFromFloats(3, this._r.x, this._r.y, this._r.z, this._r.w);
+    this._t += 81337.18;
+  }
+
+  private getModeFromString(mode: string) {
+    const lowerCaseMode = mode.toLowerCase();
+    switch (lowerCaseMode) {
+      case "single": return ShakeMode.Single;
+      case "group": return ShakeMode.Group;
+      default: return ShakeMode.Single;
+    }
+  }
+
+  private calculateModeMultiplier(mode: ShakeMode, maxAccumulatedAudio: number, audioData: AudioInput) {
+    if(mode === ShakeMode.Single) {
+      const numPoints = audioData.frequencyBinCount;
+      let highestFrequency: number = 0;
+
+      for (let i = 0; i < maxAccumulatedAudio; ++i) {
+        const ndx = i * numPoints / maxAccumulatedAudio | 0;
+        const freq = audioData.audioData[ndx] / 255.0;
+
+        if(highestFrequency < freq)
+          highestFrequency = freq;
+      }
+      return highestFrequency;
+
+    } else if (mode === ShakeMode.Group) {
+      return audioData.accumulatedAudio / maxAccumulatedAudio;
+    }
+
+    return 1;
   }
 }
