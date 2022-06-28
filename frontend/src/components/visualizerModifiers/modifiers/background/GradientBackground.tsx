b5340d4d6d97fce83f1be1cfec39402c2a2f744e
Nazar Derevetskyy <nazmouse22@hotmail.com>
2022-06-28 01:27:00 -0400

    Gradient background with multicolor picker

--- frontend/src/components/visualizerModifiers/modifiers/background/GradientBackground.tsx
+++ frontend/src/components/visualizerModifiers/modifiers/background/GradientBackground.tsx
@@ -1,3 +1,20 @@
+import { FieldAccordeon } from "../../genericProperties/fieldAccordeon/FieldAccordeon";
+import { MultiColorPicker } from "../../genericProperties/multiColorPicker/MultiColorPicker";
+import { useModifiersContext } from "../../../../contexts/ModifiersContext";
+import { Modifiers } from "../../../../Constants";
+
 export const GradientBackground = () => {
-  return (<p>hi it's gradient</p>);
+  const { data, setData } = useModifiersContext();
+  const bgData = data[Modifiers.BACKGROUND];
+
+  const onBackgroundGradientSet = (colors: string[]) => {
+    bgData["bgColors"] = colors;
+    setData({...data, data});
+  };
+
+  return (
+    <FieldAccordeon title="Color">
+      <MultiColorPicker colors={bgData["bgColors"]} setColors={onBackgroundGradientSet}/>
+    </FieldAccordeon>
+  );
 }
